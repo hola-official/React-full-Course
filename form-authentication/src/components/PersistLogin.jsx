a8fe5406bacf69398d0e552f1a9d7d6f6305b2d2
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-16 11:39:41 +0100

    commit

--- form-authentication/src/components/PersistLogin.jsx
+++ form-authentication/src/components/PersistLogin.jsx
@@ -2,3 +2,7 @@ import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 import useRefreshToken from "../hook/useRefreshToken";
 import useAuth from "../hook/useAuth";
+
+const PersistLogin = () => {
+    
+}
