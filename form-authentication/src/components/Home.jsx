c3d24696c1526f15164e3088f06ef2a02dcab30b
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-16 12:49:02 +0100

    commit

--- form-authentication/src/components/Home.jsx
+++ form-authentication/src/components/Home.jsx
@@ -4,36 +4,36 @@ import useLogout from "../hook/useLogout";
 // import AuthContext from "../context/AuthProvider";
 
 const Home = () => {
-  // const { setAuth } = useContext(AuthContext);
-  const navigate = useNavigate();
-  const logout = useLogout();
+    // const { setAuth } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const logout = useLogout();
 
-  const signOut = async () => {
-    // if used in more components, this should be in context
-    // axios to /logout endpoint
-    // setAuth({});
-    logout();
-    navigate("/linkpage");
-  };
+    const signOut = async () => {
+        // if used in more components, this should be in context
+        // axios to /logout endpoint
+        // setAuth({});
+        logout();
+        navigate("/linkpage");
+    };
 
-  return (
-    <section>
-      <h1>Home</h1>
-      <br />
-      <p>You are logged in!</p>
-      <br />
-      <Link to="/editor">Go to the Editor page</Link>
-      <br />
-      <Link to="/admin">Go to the Admin page</Link>
-      <br />
-      <Link to="/lounge">Go to the Lounge</Link>
-      <br />
-      <Link to="/linkpage">Go to the link page</Link>
-      <div className="flexGrow">
-        <button onClick={signOut}>Sign Out</button>
-      </div>
-    </section>
-  );
+    return (
+        <section>
+            <h1>Home</h1>
+            <br />
+            <p>You are logged in!</p>
+            <br />
+            <Link to="/editor">Go to the Editor page</Link>
+            <br />
+            <Link to="/admin">Go to the Admin page</Link>
+            <br />
+            <Link to="/lounge">Go to the Lounge</Link>
+            <br />
+            <Link to="/linkpage">Go to the link page</Link>
+            <div className="flexGrow">
+                <button onClick={signOut}>Sign Out</button>
+            </div>
+        </section>
+    );
 };
 
 export default Home;
