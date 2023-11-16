de908ff550bba075dc196b9e76497a6dafad749e
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-16 12:43:28 +0100

    commit

--- form-authentication/src/components/Home.jsx
+++ form-authentication/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { useNavigate, Link } from "react-router-dom";
-import { useContext } from "react";
-import AuthContext from "../context/AuthProvider";
+// import { useContext } from "react";
+// import AuthContext from "../context/AuthProvider";
 
 const Home = () => {
     // const { setAuth } = useContext(AuthContext);
