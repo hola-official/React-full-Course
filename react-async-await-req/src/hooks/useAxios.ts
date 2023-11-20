1bb790313b9bdf1d91bb8636ff511a9e404f385b
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-20 12:51:31 +0100

    commit

--- react-async-await-req/src/hooks/useAxios.ts
+++ react-async-await-req/src/hooks/useAxios.ts
@@ -1,6 +1,9 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
+const useAxios = (configObj) => {
+    const { axiosInstance, method, url, requestConfig = {} } = configObj;
 
-const useAxios = () => {
-    
-}
+    const [response, setResponse] = useState([]);
+    const [Error, setError] = useState("");
+    const [loading, setLoading] = useState(true);
+};
