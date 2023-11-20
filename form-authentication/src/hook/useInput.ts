44158c914db351e2ff847e3243e42eb78867e820
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-20 10:34:10 +0100

    commit

--- form-authentication/src/hook/useInput.ts
+++ form-authentication/src/hook/useInput.ts
@@ -2,4 +2,9 @@ import { useState, useEffect } from "react";
 
 const useInput = (initValue) => {
     const [value, setValue] = useState(initValue)
+    const reset = () => setValue(initValue)
+
+    const attributeObj = {
+        value: value,
+    }
 }
