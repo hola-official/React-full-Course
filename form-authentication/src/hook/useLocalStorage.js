a4285034cefeaa800dc2ef1aed75eff2edb2e467
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-20 09:25:35 +0100

    commit

--- form-authentication/src/hook/useLocalStorage.js
+++ form-authentication/src/hook/useLocalStorage.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 const useLocalStorage = (key, initValue) => {
-
+    const [value, setValue] = useState(JSON.parse(localStorage.getItem(key)))
 }
 
 export default useLocalStorage
