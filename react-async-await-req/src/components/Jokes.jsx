5c82d620707bdd74817f1bf30837ce64a3ab1420
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-20 14:20:50 +0100

    commit

--- react-async-await-req/src/components/Jokes.jsx
+++ react-async-await-req/src/components/Jokes.jsx
@@ -1,4 +1,9 @@
+import useAxios from "../hooks/useAxios"
+
 const Jokes = () => {
+    const [joke, error, loading] = useAxios({
+        url: 'https://official-api.codersforcauses.org/jokes'
+    })
   return (
     <div>
         <h1>Jokes</h1>
