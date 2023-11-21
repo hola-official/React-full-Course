196d38ad70698c30910f53fbd4ce022fb3e18204
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-21 08:26:30 +0100

    commit

---
+++ react-async-await-req/src/hooks/useAxiosFunction.ts
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+const useAxios = (configObj) => {
+    const { axiosInstance, method, url, requestConfig = {} } = configObj;
+
+    const [response, setResponse] = useState([]);
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [reload, setReload] = useState(0);
+
+    const refetch = () => setReload(prev => prev + 1);
+
+    useEffect(() => {
+        // const controller = new AbortController();
+        let isMounted = true;
+        const fetchData = async () => {
+            try {
+                const res = await axiosInstance[method.toLowerCase()](url, {
+                    ...requestConfig,
+                    // signal: controller.signal
+                })
+                console.log(res);
+                isMounted && setResponse(res.data)
+
+            } catch (err) {
+                console.log(err);
+                setError(err.message)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchData();
+
+        return () => {
+            isMounted = false
+            // controller.abort()
+        };
+
+    }, [reload])
+
+    return [response, loading, error, refetch]
+};
+
+
+export default useAxios
