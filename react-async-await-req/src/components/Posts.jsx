5021a302d10698970844128263dc1dfbe37dd76f
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-21 09:02:00 +0100

    commit

---
+++ react-async-await-req/src/components/Posts.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import axios from "../api/jsonPH";
+import useAxiosFunction from '../hooks/useAxiosFunction'
+
+const Posts = () => {
+    
+  return (
+    <div>Posts</div>
+  )
+}
+
+export default Posts
