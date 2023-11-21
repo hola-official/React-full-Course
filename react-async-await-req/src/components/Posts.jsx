454ffdf1b65935464473110b9ce40ec56a843c29
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-21 09:25:07 +0100

    commit

--- react-async-await-req/src/components/Posts.jsx
+++ react-async-await-req/src/components/Posts.jsx
@@ -1,46 +1,44 @@
 import { useEffect } from "react";
 import axios from "../api/jsonPH";
-import useAxiosFunction from '../hooks/useAxiosFunction'
+import useAxiosFunction from "../hooks/useAxiosFunction";
 
 const Posts = () => {
-    const [response, loading, error, axiosFetch] = useAxiosFunction();
+  const [response, loading, error, axiosFetch] = useAxiosFunction();
 
-    // const getData = () => {
-    //     axiosFetch({
-    //         axiosInstance: axios,
-    //         url: '/posts',
-    //         method: 'GET',
-    //     })
-    // }
+  // const getData = () => {
+  //     axiosFetch({
+  //         axiosInstance: axios,
+  //         url: '/posts',
+  //         method: 'GET',
+  //     })
+  // }
 
-    useEffect(() => {
-        // To get data from the endpoint
-        axiosFetch({
-            axiosInstance: axios,
-            url: '/posts',
-            method: 'GET',
-        })
-        // getData();
-    }, []);
+  useEffect(() => {
+    // To get data from the endpoint
+    axiosFetch({
+      axiosInstance: axios,
+      url: "/posts",
+      method: "GET",
+    });
+    // getData();
+  }, []);
 
-    const handleSubmit = () => {
-        axiosFetch({
-            axiosInstance: axios,
-            url: '/posts',
-            method: 'POST',
-            requestConfig: {
-                data: {
-                    "userId": 1,
-                    "id": 1,
-                    "title": "Supreme",
-                    "body": "Supremacy of the law..."
-                  }
-            }
-        })
-    }
-    return (
-        <div>Posts</div>
-    )
-}
+  const handleSubmit = () => {
+    axiosFetch({
+      axiosInstance: axios,
+      url: "/posts",
+      method: "POST",
+      requestConfig: {
+        data: {
+          userId: 1,
+          id: 1,
+          title: "Supreme",
+          body: "Supremacy of the law...",
+        },
+      },
+    });
+  };
+  return <div>Posts</div>;
+};
 
-export default Posts
+export default Posts;
