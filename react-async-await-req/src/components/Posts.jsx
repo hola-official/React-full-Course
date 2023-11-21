e7d68283a32cb05da17bee163a48e253e5c1c89d
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-21 09:28:40 +0100

    commit

--- react-async-await-req/src/components/Posts.jsx
+++ react-async-await-req/src/components/Posts.jsx
@@ -3,54 +3,54 @@ import axios from "../api/jsonPH";
 import useAxiosFunction from "../hooks/useAxiosFunction";
 
 const Posts = () => {
-  const [response, loading, error, axiosFetch] = useAxiosFunction();
+    const [response, loading, error, axiosFetch] = useAxiosFunction();
 
-  // const getData = () => {
-  //     axiosFetch({
-  //         axiosInstance: axios,
-  //         url: '/posts',
-  //         method: 'GET',
-  //     })
-  // }
+    // const getData = () => {
+    //     axiosFetch({
+    //         axiosInstance: axios,
+    //         url: '/posts',
+    //         method: 'GET',
+    //     })
+    // }
 
-  useEffect(() => {
-    // To get data from the endpoint
-    axiosFetch({
-      axiosInstance: axios,
-      url: "/posts",
-      method: "GET",
-    });
-    // getData();
-  }, []);
+    useEffect(() => {
+        // To get data from the endpoint
+        axiosFetch({
+            axiosInstance: axios,
+            url: "/posts",
+            method: "GET",
+        });
+        // getData();
+    }, []);
 
-  const handleSubmit = () => {
-    axiosFetch({
-      axiosInstance: axios,
-      url: "/posts",
-      method: "POST",
-      requestConfig: {
-        data: {
-          userId: 1,
-          id: 1,
-          title: "Supreme",
-          body: "Supremacy of the law...",
-        },
-      },
-    });
-  };
-  return (
-    <article>
-      <h2>Random Dad Jokes.</h2>
+    const handleSubmit = () => {
+        axiosFetch({
+            axiosInstance: axios,
+            url: "/posts",
+            method: "POST",
+            requestConfig: {
+                data: {
+                    userId: 1,
+                    id: 1,
+                    title: "Supreme",
+                    body: "Supremacy of the law...",
+                },
+            },
+        });
+    };
+    return (
+        <article>
+            <h2>Random Dad Jokes.</h2>
 
-      {loading && <p>loading...</p>}
+            {loading && <p>loading...</p>}
 
-      {!loading && error && <p className="errMsg">{error}</p>}
+            {!loading && error && <p className="errMsg">{error}</p>}
 
-      {!loading && !error && joke && <p>{joke?.joke}</p>}
+            {!loading && !error && joke && <p>{joke?.joke}</p>}
 
-      {!loading && !error && !joke && <p>No Joke display! 🙂</p>}
-    </article>
-  );
+            {!loading && !error && !joke && <p>No Joke display! 🙂</p>}
+        </article>
+    );
 };
 
 export default Posts;
