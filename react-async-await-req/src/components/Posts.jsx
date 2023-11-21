18b1fda5369e0872c43ce1e852a806f997abca91
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-21 09:07:22 +0100

    commit

--- react-async-await-req/src/components/Posts.jsx
+++ react-async-await-req/src/components/Posts.jsx
@@ -3,8 +3,11 @@ import axios from "../api/jsonPH";
 import useAxiosFunction from '../hooks/useAxiosFunction'
 
 const Posts = () => {
-    const [response, loading, error, axiosFetch] = useAxiosFunction;
+    const [response, loading, error, axiosFetch] = useAxiosFunction();
 
+    const getData = () => {
+
+    }
     return (
         <div>Posts</div>
     )
