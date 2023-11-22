d971db9b40d5d33a0114e723a61c1478ff34cc8a
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-22 14:09:06 +0100

    commit

--- react-infinite-scroll/src/Example1.jsx
+++ react-infinite-scroll/src/Example1.jsx
@@ -34,7 +34,7 @@ const Example1 = () => {
     intObserver.current = new IntersectionObserver((post) => {
       if (post[0].isIntersecting && hasNextPage) {
         console.log('we are near the last post');
-        setPageNum(prev => prev + 1)
+        confirmedLoad()
       }
     })
     if (post) intObserver.current.observe(post)
