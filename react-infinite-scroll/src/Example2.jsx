54751cc4d75c218d757751266c1ca81b61e84a84
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-22 14:24:09 +0100

    commit

--- react-infinite-scroll/src/Example2.jsx
+++ react-infinite-scroll/src/Example2.jsx
@@ -1,71 +1,72 @@
-import { useRef, useCallback } from "react"
-import { confirmAlert } from "react-confirm-alert"
-import 'react-confirm-alert/src/react-confirm-alert.css'
-import Post from "./Post"
-import { UseInfiniteQuery } from "react-query"
-import { getPostPage } from "./api/axios"
+import { useRef, useCallback } from "react";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
+import Post from "./Post";
+import { UseInfiniteQuery } from "react-query";
+import { getPostPage } from "./api/axios";
 
 const Example2 = () => {
-    const [pageNum, setPageNum] = useState(1)
-    const { loading, isError, error, results, hasNextPage } = usePosts(pageNum)
+  const [pageNum, setPageNum] = useState(1);
+  const { loading, isError, error, results, hasNextPage } = usePosts(pageNum);
 
-    const intObserver = useRef();
+  const intObserver = useRef();
 
-    const lastPostRef = useCallback((post) => {
-        if (loading) return;
+  const lastPostRef = useCallback(
+    (post) => {
+      if (loading) return;
 
-        if (intObserver.current) return intObserver.current.disconnect();
+      if (intObserver.current) return intObserver.current.disconnect();
 
-        const confirmedLoad = () => {
-            confirmAlert({
-                title: 'Confirm Load More',
-                message: 'Are you sure to load more posts?',
-                buttons: [
-                    {
-                        label: 'Yes',
-                        onClick: () => {
-                            setPageNum((prev) => prev + 1);
-                        }
-                    },
-                    { label: 'No' }
-                ]
-            })
+      const confirmedLoad = () => {
+        confirmAlert({
+          title: "Confirm Load More",
+          message: "Are you sure to load more posts?",
+          buttons: [
+            {
+              label: "Yes",
+              onClick: () => {
+                setPageNum((prev) => prev + 1);
+              },
+            },
+            { label: "No" },
+          ],
+        });
+      };
+
+      intObserver.current = new IntersectionObserver((post) => {
+        if (post[0].isIntersecting && hasNextPage) {
+          console.log("we are near the last post");
+          confirmedLoad();
         }
+      });
+      if (post) intObserver.current.observe(post);
+    },
+    [loading, hasNextPage]
+  );
 
-        intObserver.current = new IntersectionObserver((post) => {
-            if (post[0].isIntersecting && hasNextPage) {
-                console.log('we are near the last post');
-                confirmedLoad()
-            }
-        })
-        if (post) intObserver.current.observe(post)
-    }, [loading, hasNextPage])
+  if (isError) return <p className="center">Error: {error.message}</p>;
 
-    if (isError) return <p className="center">Error: {error.message}</p>
+  const content = results.map((post, i) => {
+    if (results.length === i + 1) {
+      return <Post ref={lastPostRef} key={post.id} post={post} />;
+    }
+    return <Post key={post.id} post={post} />;
+  });
+  return (
+    <>
+      <h1 id="top">
+        &infin; Infinite Query &amp; Scroll
+        <br />
+        &infin; Ex. 1 - React Only
+      </h1>
+      {content}
+      <p className="center">Loading more posts...</p>
 
-    const content = results.map((post, i) => {
-        if (results.length === i + 1) {
-            return <Post ref={lastPostRef} key={post.id} post={post} />
-        }
-        return <Post key={post.id} post={post} />
-    })
-    return (
-        <>
-            <h1 id="top">
-                &infin; Infinite Query &amp; Scroll
-                <br />
-                &infin; Ex. 1 - React Only
-            </h1>
-            {content}
-            <p className="center">
-                Loading more posts...
-            </p>
+      <p className="center">
+        <a href="#top">Back to Top</a>
+      </p>
+    </>
+  );
+};
 
-            <p className="center">
-                <a href="#top">Back to Top</a>
-            </p>
-        </>
-    )
-}
-
-export default Example2
+export default Example2;
