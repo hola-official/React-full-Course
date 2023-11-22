0e5738c416b63c02c12be7b709624872bad3df8a
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-22 14:36:17 +0100

    commit

--- react-infinite-scroll/src/Example2.jsx
+++ react-infinite-scroll/src/Example2.jsx
@@ -7,7 +7,9 @@ import { getPostPage } from "./api/axios";
 
 const Example2 = () => {
     const { fetchNextPage, hasNextPage, isFetchingNextPage, data, status, error } = useInfiniteQuery('/posts', ({pageParam = 1}) => getPostPage(pageParam), {
-        getNextPageParam: 
+        getNextPageParam: (lastPage, allPages) => {
+            return lastPage.length ? allPages.length + 1 : undefined
+        }
     })
     const intObserver = useRef();
 
