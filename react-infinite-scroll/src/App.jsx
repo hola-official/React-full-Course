9f8a81cad56308957429fc5a830177a675a70846
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-22 10:02:53 +0100

    commit

--- react-infinite-scroll/src/App.jsx
+++ react-infinite-scroll/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Example1 from './Example1'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <main className='App'>
+      <Example1 />
+    </main>
   )
 }
 
