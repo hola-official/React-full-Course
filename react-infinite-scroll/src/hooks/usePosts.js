0946437287cd6ce4fad8c2440aae986a87f05081
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-22 10:41:34 +0100

    commit

--- react-infinite-scroll/src/hooks/usePosts.js
+++ react-infinite-scroll/src/hooks/usePosts.js
@@ -17,16 +17,17 @@ const usePosts = (pageNum = 1) => {
     const { signal } = controller
 
     getPostPage(pageNum, { signal })
-    .then(data => {
-      setResults(prev => [...prev, ...data]);
-      setHasNextPage(Boolean(data.length));
-      setLoading(false)
-    })
-    .catch(e => {
-      setLoading(false)
-      if(signal.aborted) return 
-      setIsError(true)
-    })
+      .then(data => {
+        setResults(prev => [...prev, ...data]);
+        setHasNextPage(Boolean(data.length));
+        setLoading(false)
+      })
+      .catch(e => {
+        setLoading(false)
+        if (signal.aborted) return
+        setIsError(true)
+        setError({ message: e.message })
+      })
   }, []);
 
   return { loading, isError, error, results, hasNextPage };
