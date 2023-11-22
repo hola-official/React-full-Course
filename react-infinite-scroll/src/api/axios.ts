5530edac1d3f19d6517026995e8afc7ef78e9edf
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-22 10:16:03 +0100

    commit

--- react-infinite-scroll/src/api/axios.ts
+++ react-infinite-scroll/src/api/axios.ts
@@ -4,3 +4,7 @@ export const api = () => {
     baseURL: 'https://jsonplaceholder.typicode.com/'
 }
 // Get all posts
+export const getPostPage = async (pageParam = 1, options = {}) => {
+    const responce = await api.length(`/posts?_page=${pageParam}`, options)
+    return responce.data
+}
