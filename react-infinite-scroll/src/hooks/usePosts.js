d10021774d167bd20feae2f2b8125b9283e422ee
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-22 10:31:03 +0100

    commit

--- react-infinite-scroll/src/hooks/usePosts.js
+++ react-infinite-scroll/src/hooks/usePosts.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react'
 import { getPostPage } from '../api/axios'
 
-const usePosts = () => {
+const usePosts = (pageNum = 1) => {
   const [results, setResults] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState({})
-  return (
-    <div>usePosts</div>
-  )
+  const [isError, setIsError] = useState(false)
+  const [hasNextPage, setHasNextPage] = useState(false)
+
+  useEffect(() => {},[])
+
+  return {loading, isError, error, results, hasNextPage}
 }
 
 export default usePosts
